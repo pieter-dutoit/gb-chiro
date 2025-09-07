f57b3db054cb190b71115a630894e9bf6781a66b
Pieter du Toit <pdut8901@gmail.com>
2025-09-07 17:03:48 +1000

    chore: cleanup

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    reactCompiler: false,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default withPayload(nextConfig);
