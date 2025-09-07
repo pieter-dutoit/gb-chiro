23dd02775cdee0d6d71ab6fd02d40754371e1ee0
Pieter du Toit <pdut8901@gmail.com>
2025-09-07 17:16:32 +1000

    chore: cleanup

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  turbopack: {
+    resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".mjs", ".json"],
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+};
 
 export default withPayload(nextConfig);
--- src/app/(app)/images/[fileName]/route.ts
+++ src/app/(app)/images/[fileName]/route.ts
@@ -11,38 +11,50 @@ export async function GET(
   _request: Request,
   { params }: { params: Promise<{ fileName: string }> }
 ) {
+  console.log("0");
   try {
+    console.log("1");
     const { fileName } = await params;
+    console.log("2");
     const origin = originFromEnv();
+    console.log("3");
 
     const upstreamUrl = `${origin}/api/media/file/${encodeURIComponent(fileName)}`;
+    console.log("4");
+    console.log("upstream: ", upstreamUrl);
     const upstream = await fetch(upstreamUrl, {
       cache: "force-cache",
     });
+    console.log("5");
 
     if (!upstream.ok) {
+      console.log("6");
       return new Response(
         `Failed to fetch image ${fileName} from ${upstreamUrl}: ${upstream.statusText}`,
         { status: upstream.status }
       );
     }
-
+    console.log("7");
     const contentType = upstream.headers.get("Content-Type") || "";
     if (!contentType.startsWith("image/")) {
+      console.log("8");
       throw new Error(`Invalid image content type: ${contentType}`);
     }
+    console.log("9");
 
     const headers = new Headers();
     headers.set("Content-Type", contentType);
     headers.set("Cache-Control", "public, max-age=31536000, immutable");
-
+    console.log("10");
     return new Response(upstream.body, {
       status: upstream.status,
       statusText: upstream.statusText,
       headers,
     });
   } catch (error) {
+    console.log("12");
     console.error(error);
     return new Response("Internal Server Error", { status: 500 });
   }
+  console.log("12");
 }
