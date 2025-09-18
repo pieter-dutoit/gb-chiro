5fd4eaffd79a055768ae609094f2a6ea2cfd03f6
Pieter du Toit <pdut8901@gmail.com>
2025-09-18 15:29:15 +1000

    chore: cleanup

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/admin/", "/api/media/"],
+      disallow: ["/admin/", "/api/"],
     },
     sitemap: `${getBaseUrl()}/sitemap.xml`,
   };
