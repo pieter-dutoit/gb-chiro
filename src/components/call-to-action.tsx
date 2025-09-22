4994797f7ae25cc666b9d25cfc928904d8b99f01
Pieter du Toit <pdut8901@gmail.com>
2025-09-22 18:16:02 +1000

    fix: map behind image

--- src/components/call-to-action.tsx
+++ src/components/call-to-action.tsx
@@ -42,15 +42,16 @@ export default async function CallToAction({
       >
         {/* Graphic */}
         <div
+          tabIndex={-1}
           className={twMerge(
-            "absolute h-full top-0 left-1/12 w-1/2 sm:w-1/4 lg:w-2/12 overflow-hidden scale-110 ",
+            "pointer-events-none -z-0 absolute h-full top-0 left-1/12 w-1/2 sm:w-1/4 lg:w-2/12 overflow-hidden scale-110 ",
             withMap ? "opacity-5" : "opacity-6 lg:opacity-10"
           )}
         >
           <CMSImage media={backgroundGraphic} sizes="" />
         </div>
         {/* Container */}
-        <div className="flex flex-col container mx-auto px-4 md:px-12 py-16 lg:py-24 xl:py-30 gap-8 lg:gap-12 relative">
+        <div className="z-10 flex flex-col container mx-auto px-4 md:px-12 py-16 lg:py-24 xl:py-30 gap-8 lg:gap-12 relative">
           {/* Heading */}
           <div className="flex flex-col items-center text-center gap-4">
             <Typography as="h2" variant="miniHeading" tone="light">
@@ -84,7 +85,7 @@ export default async function CallToAction({
         </div>
 
         {withMap && (
-          <div className="w-full p-8 h-[26rem] mt-4">
+          <div className="w-full p-8 h-[26rem] mt-4 z-10">
             <iframe
               title="Map Location"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3298.597713281765!2d146.0460939757263!3d-34.281409573071585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b1fae72bfdebe37%3A0xdeaaad55252d45cf!2s2%20Noorebar%20Ave%2C%20Griffith%20NSW%202680!5e0!3m2!1sen!2sau!4v1757483283999!5m2!1sen!2sau"
